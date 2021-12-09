7a97f596b4bdd914e02b52dc2c2caf5c0ddfb74f
jeet-ss <jeet.sensarma@gmail.com>
2021-12-09 19:48:06 +0100

    final 2

--- exercise2_material/src_to_implement/Layers/Conv.py
+++ exercise2_material/src_to_implement/Layers/Conv.py
@@ -22,20 +22,12 @@ class Conv(BaseLayer):
         self.num_kernels = num_kernels  # integer val
         self.convolution_shape = convolution_shape  # decides 1D or 2D convolution
 
-        # self.weights = np.linspace(-1.0, 1.0, num=np.prod(weight_shape), dtype='float64').reshape(weight_shape)
-        '''
-        if len(convolution_shape) == 1:
-            convolution_shape_1D = np.append(convolution_shape, [1])
-            self.convolution_shape = tuple(convolution_shape_1D)
-        '''
         self.weights_shape = (num_kernels,) + self.convolution_shape
         self.weights = np.random.uniform(0.0, 1.0, self.weights_shape)
         # self.weights = np.random.uniform(0.0,1.0, (np.concatenate(([self.num_kernels], self.convolution_shape))))
         # weight_shape = (num_kernels, self.convolution_shape)
         self.bias = np.random.randn(num_kernels)
 
-        #self.gradient_weights = np.zeros(self.weights.shape)
-        #self.gradient_bias = np.random.randn(num_kernels)
         self.gradient_weights = None
         self.gradient_bias = None
 
@@ -65,9 +57,7 @@ class Conv(BaseLayer):
                     layer = 0
                 else:
                     layer = math.floor(self.convolution_shape[0] / 2)
-                # w = self.weights[i]
                 forward_conv = signal.correlate(image, self.weights[i], mode='same')[layer] + self.bias[i]
-                #forward_conv1 = np.sum(forward_conv, axis=0)
                 # stack up the features from each kernel
                 features = np.append(features, [forward_conv], axis=0)
             # stack up features for each image
@@ -75,9 +65,9 @@ class Conv(BaseLayer):
         # get rid of the layer with only zeros
         feature_map = feature_map[1::]
         # Strides
-        if len(self.stride_shape) == 2:
+        if len(self.stride_shape) == 2: # 2D
             feature_map = feature_map[:, :, 0::self.stride_shape[0], 0::self.stride_shape[1]]
-        elif len(self.stride_shape) == 1:
+        elif len(self.stride_shape) == 1: # 1D
             feature_map = feature_map[:, :, 0::self.stride_shape[0]]
 
         return feature_map
@@ -85,14 +75,14 @@ class Conv(BaseLayer):
     def backward(self, error_tensor):
         # Up-sampling
         error_empty = np.zeros(np.concatenate(((error_tensor.shape[0], error_tensor.shape[1]), self.image_size)))
-        if np.ndim(error_tensor) == 4:
+        if np.ndim(error_tensor) == 4: # 2D
             error_empty[:, :, 0::self.stride_shape[0], 0::self.stride_shape[1]] = error_tensor
         elif np.ndim(error_tensor) == 3:  # 1D case
             error_empty[:, :, 0::self.stride_shape[0]] = error_tensor
         self.error_tensor = error_empty
+
         # reshaping weights
         back_weights = np.copy(self.weights)
-
         back_weights = np.swapaxes(back_weights, 0, 1)  # swap kernel no and channel
         if np.ndim(self.weights) == 4:
             back_weights = np.flip(back_weights, 1)  # flip along channels
@@ -109,7 +99,6 @@ class Conv(BaseLayer):
                 else:
                     layer_indx = math.floor(back_weights.shape[1] / 2)
                 back_conv = signal.convolve(layer, self.back_weights[i], mode='same')[layer_indx]
-                #back_conv = np.sum(back_conv, axis=0)
                 features_back = np.append(features_back, [back_conv], axis=0)
             feature_map_back = np.append(feature_map_back, [features_back[1::]], axis=0)
         feature_map_back = feature_map_back[1::]
@@ -123,35 +112,35 @@ class Conv(BaseLayer):
             if float.is_integer(pad_y):
                 pad_y1 = math.floor(pad_y)
                 pad_y2 = math.floor(pad_y)
-            else:
+            else:   # for even kernels
                 pad_y1 = math.floor(pad_y)
                 pad_y2 = math.floor(pad_y) + 1
             # check for x
             if float.is_integer(pad_x):
                 pad_x1 = math.floor(pad_x)
                 pad_x2 = math.floor(pad_x)
-            else:
+            else:  # for even kernel
                 pad_x1 = math.floor(pad_x)
                 pad_x2 = math.floor(pad_x) + 1
             padded_image = np.pad(self.input_tensor, ((0, 0), (0, 0), (pad_y1, pad_y2), (pad_x1, pad_x2)), 'constant',
                                   constant_values=0.0)
-        elif np.ndim(self.weights) == 3:
+        elif np.ndim(self.weights) == 3:  # 1D case
             padded_image = self.input_tensor
         # correlate part
         self.gradient_weights = np.zeros(self.weights.shape)
-        if np.ndim(self.weights) == 4:
+        if np.ndim(self.weights) == 4:  # 2D case
             # bias gradient
             self.gradient_bias = np.sum(np.sum(np.sum(self.error_tensor, axis=3), axis=2), axis=0)
             # weights gradient
             gradient_weights = np.copy(self.gradient_weights)
-            for i in range(self.input_tensor.shape[0]):  # 2
-                for j in range(self.error_tensor.shape[1]):  # 4
-                    for k in range(self.input_tensor.shape[1]):  # 3
+            for i in range(self.input_tensor.shape[0]):  # batch size
+                for j in range(self.error_tensor.shape[1]):  # error tensor channel no / no of filters in forward
+                    for k in range(self.input_tensor.shape[1]):  # input tensor channel no / no of filters in backward path
                         gradient_weights[j, k, :, :] += signal.correlate(padded_image[i, k, :, :],
                                                                          self.error_tensor[i, j, :, :], mode='valid')
 
             self.gradient_weights = gradient_weights
-        elif np.ndim(self.weights) == 3:
+        elif np.ndim(self.weights) == 3:  # 1D case
             # bias
             self.gradient_bias = np.sum(np.sum(self.error_tensor, axis=2), axis=0)
             # weights
