d03581a4b328c4c13d47f451e12c7a53eb95883e
jeet-ss <jeet.sensarma@gmail.com>
2021-12-09 14:51:07 +0100

    backward pass 3

--- exercise2_material/src_to_implement/Layers/Conv.py
+++ exercise2_material/src_to_implement/Layers/Conv.py
@@ -10,6 +10,12 @@ class Conv(BaseLayer):
 
     def __init__(self, stride_shape, convolution_shape, num_kernels):
         super().__init__()
+        self._optimizerBias = None
+        self._optimizerWeights = None
+        self._optimizer = None
+        self._gradient_bias = None
+        self._gradient_weights = None
+
         self.trainable = True
 
         self.stride_shape = stride_shape  # single val or tuple
@@ -67,11 +73,9 @@ class Conv(BaseLayer):
             feature_map = feature_map[:, :, 0::self.stride_shape[0], 0::self.stride_shape[1]]
         elif len(self.stride_shape) == 1:
             feature_map = feature_map[:, :, 0::self.stride_shape[0]]
-        print("forwrad out = ", feature_map.shape)
         return feature_map
 
     def backward(self, error_tensor):
-        print("error_tensor", error_tensor.shape)
         # Up-sampling
         error_empty = np.zeros(np.concatenate(((error_tensor.shape[0], error_tensor.shape[1]), self.image_size)))
         if np.ndim(error_tensor) == 4:
@@ -79,20 +83,15 @@ class Conv(BaseLayer):
         elif np.ndim(error_tensor) == 3:  # 1D case
             error_empty[:, :, 0::self.stride_shape[0]] = error_tensor
         self.error_tensor = error_empty
-        print("input", self.input_tensor.shape)
-        print("error", self.error_tensor.shape)
         # reshaping weights
-        # print("fw", self.weights.shape)
         back_weights = np.copy(self.weights)
         back_weights = np.swapaxes(back_weights, 0, 1)  # swap kernel no and channel
         back_weights = np.flip(back_weights, 1)  # flip along channels
         self.back_weights = back_weights
-        # print("bw" , self.back_weights.shape)
 
         # gradient wrt lower layer
         feature_map_back = np.zeros((np.concatenate(((1, self.back_weights.shape[0]), self.image_size))))
         for layer in self.error_tensor:
-            # print("la", layer.shape)
             features_back = np.zeros((np.concatenate(((1,), self.image_size))))
             for i in range(self.back_weights.shape[0]):
                 back_conv = signal.convolve(layer, self.back_weights[i], mode='same')
@@ -100,16 +99,12 @@ class Conv(BaseLayer):
                 features_back = np.append(features_back, [back_conv], axis=0)
             feature_map_back = np.append(feature_map_back, [features_back[1::]], axis=0)
         feature_map_back = feature_map_back[1::]
-        print(feature_map_back.shape)
 
         # gradient wrt weights
         back_features = np.copy(feature_map_back)
-        # print("before pad", back_features.shape)
-        # padding calculation
         if np.ndim(self.weights) == 4:
             pad_y = ((self.weights.shape[2] - 1) / 2)
             pad_x = ((self.weights.shape[3] - 1) / 2)
-            print(pad_x, pad_y)
             # check for y
             if float.is_integer(pad_y):
                 pad_y1 = math.floor(pad_y)
@@ -124,15 +119,12 @@ class Conv(BaseLayer):
             else:
                 pad_x1 = math.floor(pad_x)
                 pad_x2 = math.floor(pad_x) + 1
-            print(pad_x1, pad_x2, pad_y1, pad_y2)
             padded_image = np.pad(self.input_tensor, ((0, 0), (0, 0), (pad_y1, pad_y2), (pad_x1, pad_x2)), 'constant',
                                   constant_values=0.0)
-            print("after pad", padded_image.shape)
         elif np.ndim(self.weights) == 3:
             padded_image = self.input_tensor
         # correlate part
         self.gradient_weights = np.zeros(self.weights.shape)
-        print("grad", self.gradient_weights.shape)
         if np.ndim(self.weights) == 4:
             # bias gradient
             self.gradient_bias = np.sum(np.sum(np.sum(self.error_tensor, axis=3), axis=2), axis=0)
@@ -141,15 +133,10 @@ class Conv(BaseLayer):
             for i in range(self.input_tensor.shape[0]):  # 2
                 for j in range(self.error_tensor.shape[1]):  # 4
                     for k in range(self.input_tensor.shape[1]):  # 3
-                        print(padded_image[i, k, :, :].shape, self.error_tensor[i, j, :, :].shape)
-                        # left working here
-                        # check that the weights matrix init for the back pass is not
-                        # matching and should be realted with something else
                         gradient_weights[j, k, :, :] += signal.correlate(padded_image[i, k, :, :],
                                                                          self.error_tensor[i, j, :, :], mode='valid')
 
             self.gradient_weights = gradient_weights
-            print("res", self.gradient_weights.shape)
         elif np.ndim(self.weights) == 3:
             # bias
             self.gradient_bias = np.sum(np.sum(self.error_tensor, axis=2), axis=0)
@@ -158,13 +145,12 @@ class Conv(BaseLayer):
             for i in range(self.input_tensor.shape[0]):
                 for j in range(self.num_kernels):
                     for k in range(self.gradient_weights.shape[1]):
-                        gradient_weights[j, k, :] += signal.correlate(padded_image[i, k, :],
-                                                                         self.error_tensor[i, j, :], mode='valid')
+                        gradient_weights[j, k, :] += signal.correlate(padded_image[i, k, :], self.error_tensor[i, j, :], mode='valid')
             self.gradient_weights = gradient_weights
 
         if self.optimizer:
-            self.weights = self.optimizerWeigths.calculate_update(self.weights, self.gradient_weights)
-            self.bias = self.optimizerBias.calculate_update(self.bias, self.gradient_bias)
+            self.weights = self._optimizerWeights.calculate_update(self.weights, self.gradient_weights)
+            self.bias = self._optimizerBias.calculate_update(self.bias, self.gradient_bias)
 
         return feature_map_back
 
@@ -172,18 +158,18 @@ class Conv(BaseLayer):
         self.weights = weights_initializer.initialize(self.weights.shape,
                                                       np.prod(self.convolution_shape),
                                                       self.num_kernels * np.prod(self.convolution_shape[1::]))
-        self.bias = bias_initializer.initialize(self.bias.shape,
-                                                np.prod(self.convolution_shape),
-                                                np.prod(self.convolution_shape))
+        self.bias = bias_initializer.initialize(self.bias.shape,np.prod(self.bias.shape), np.prod(self.bias.shape))
+        #self.bias = bias_initializer.initialize(self.bias.shape, np.prod(self.convolution_shape), np.prod(self.convolution_shape))
+        #self.bias = bias_initializer.initialize(self.num_kernels, np.prod(self.convolution_shape), self.num_kernels * np.prod(self.convolution_shape[1::]))
 
     @property
     def optimizer(self):
-        return self._optimizerWeigths is not  None and self._optimizerWeigths is not None
+        return self._optimizer
 
     @optimizer.setter
     def optimizer(self, optimizer):
         self._optimizer = optimizer
-        self._optimizerWeigths = copy.deepcopy(optimizer)
+        self._optimizerWeights = copy.deepcopy(optimizer)
         self._optimizerBias = copy.deepcopy(optimizer)
 
     @property
